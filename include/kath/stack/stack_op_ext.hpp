1608f224d86ae3ab6e90fb126cfb4bd427803458
Madokakaroto <IndignentAngel@gmail.com>
2018-06-07 16:43:20 +0800

    extension for sequence container and associative container

--- include/kath/stack/stack_op_ext.hpp
+++ include/kath/stack/stack_op_ext.hpp
@@ -1,1 +1,18 @@
 #pragma once
+
+namespace kath
+{
+    template <typename T, typename = void>
+    struct is_sequence_container : std::false_type {};
+
+    template <typename T>
+    struct is_sequence_container<T, std::void_t<
+        decltype(std::declval<T>().size()),
+        
+    >> : std::true_type {};
+}
+
+namespace kath { namespace ext
+{
+
+} }
