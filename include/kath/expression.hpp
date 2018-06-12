28dbad8423529e6b1f6e7a518e57a2ace35c8b07
Madokakaroto <IndigentAngel@gmail.com>
2018-06-12 09:05:37 +0800

    table operation

--- include/kath/expression.hpp
+++ include/kath/expression.hpp
@@ -25,51 +25,13 @@ namespace kath
 		template <typename Key>
 		auto access_field(lua_State* L, Key&& key) const noexcept
 		{
-			using index_expression_t = index_expression<Expr, exract_key_type_t<Key>>;
-			using table_proxy_t = table_proxy<index_expression_t>;
-			return table_proxy_t{ L, index_expression_t{ extrac_expression(*this), std::forward<Key>(key) } };
+			using forward_type = exract_key_type_t<Key>;
+			using table_proxy_t = table_proxy<Expr, forward_type>;
+			return table_proxy_t{ L, extrac_expression(*this), std::forward<forward_type>(key) };
 		}
 
 	protected:
 		base_expression() = default;
-	};
-
-	template <typename Expr>
-	class table_proxy
-	{
-	public:
-		static_assert(negative_v<meta_or<std::is_pointer<Expr>, std::is_reference<Expr>>>);
-		static_assert(std::is_base_of_v<base_expression<Expr>, Expr>);
-
-		table_proxy(lua_State* L, Expr const& expr) noexcept
-			: L(L)
-			, expr_(expr)
-		{
-		}
-
-		template <typename Value>
-		operator Value() const
-		{
-			expr_.fetch(L);
-			return kath::stack_get<Value>(L);
-		}
-
-		template <typename Value>
-		table_proxy& operator= (Value&& v)
-		{
-			expr_.set_field(L, std::forward<Value>(v));
-			return *this;
-		}
-
-		template <typename Key>
-		auto operator[](Key&& key) const noexcept
-		{
-			return expr_.access_field(L, std::forward<Key>(key));
-		}
-
-	private:
-		lua_State*		L;
-		Expr const&		expr_;
 	};
 
 	template <typename Expr, typename Key>
@@ -88,10 +50,7 @@ namespace kath
 			, key_(std::move(key))
 		{}
 
-		~index_expression()
-		{
-
-		}
+		~index_expression() = default;
 
 		void fetch(lua_State* L) const
 		{
@@ -121,5 +80,45 @@ namespace kath
 	private:
 		const_expression&	expr_;
 		key_type			key_;
+	};
+
+	template <typename Expr, typename Key>
+	class table_proxy
+	{
+	public:
+		static_assert(negative_v<meta_or<std::is_pointer<Expr>, std::is_reference<Expr>>>);
+		static_assert(std::is_base_of_v<base_expression<Expr>, Expr>);
+
+		using index_expression_t = index_expression<Expr, Key>;
+
+		table_proxy(lua_State* L, Expr const& expr, Key key) noexcept
+			: L(L)
+			, expr_(expr, std::forward<Key>(key))
+		{
+		}
+
+		template <typename Value>
+		operator Value() const
+		{
+			expr_.fetch(L);
+			return kath::stack_get<Value>(L);
+		}
+
+		template <typename Value>
+		table_proxy& operator= (Value&& v)
+		{
+			expr_.set_field(L, std::forward<Value>(v));
+			return *this;
+		}
+
+		template <typename Key>
+		auto operator[](Key&& key) const noexcept
+		{
+			return expr_.access_field(L, std::forward<Key>(key));
+		}
+
+	private:
+		lua_State *			L;
+		index_expression_t	expr_;
 	};
 }
